8ebb15c6983ce0e1dde35ca66f8bb39480eaedfd
duetkhan <bulbulahmedriyan24@gmail.com>
2026-01-24 07:45:16 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,82 +1,85 @@
 let balance = 0;
-let watched = 0;
+let watchCount = 0;
+let timer;
+let timeLeft = 15;
 
-const watchBtn = document.getElementById("watchBtn");
-const clickBtn = document.getElementById("clickBtn");
-const clickBox = document.getElementById("clickBox");
+function watchAd(){
+  startTimer();
+  // Monetag In-App / Interstitial
+  show_10497540({
+    type:'inApp'
+  });
+}
 
-const modal = document.getElementById("modal");
-const timerText = document.getElementById("timerText");
-const closeBtn = document.getElementById("closeBtn");
+function clickThisAd(){
+  show_10497540().then(()=>{
+    balance += 5;
+    document.getElementById("balance").innerText = balance;
+    watchCount = 0;
+    document.getElementById("clickBtn").style.display="none";
+    document.getElementById("watchBtn").style.display="block";
+    alert("You earned 5 TK!");
+  });
+}
 
-function startAdTimer(callback){
-  modal.style.display="block";
-  closeBtn.disabled=true;
-  let sec = 15;
-  timerText.innerText = sec + " seconds...";
-  let t = setInterval(()=>{
-    sec--;
-    timerText.innerText = sec + " seconds...";
-    if(sec<=0){
-      clearInterval(t);
-      closeBtn.disabled=false;
-      timerText.innerText="You can close now";
-      closeBtn.onclick=()=>{
-        modal.style.display="none";
-        callback();
-      }
+function startTimer(){
+  timeLeft = 15;
+  document.getElementById("timerModal").style.display="flex";
+  document.getElementById("closeTimerBtn").disabled = true;
+  document.getElementById("timerText").innerText = timeLeft;
+
+  timer = setInterval(()=>{
+    timeLeft--;
+    document.getElementById("timerText").innerText = timeLeft;
+    if(timeLeft<=0){
+      clearInterval(timer);
+      document.getElementById("closeTimerBtn").disabled = false;
     }
   },1000);
 }
 
-// WATCH AD
-watchBtn.onclick = ()=>{
-  showPopAd();
-  startAdTimer(()=>{
-    watched++;
-    if(watched>=20){
-      watchBtn.style.display="none";
-      clickBox.style.display="block";
-    }
-  });
+function closeTimer(){
+  document.getElementById("timerModal").style.display="none";
+  watchCount++;
+
+  if(watchCount >= 20){
+    document.getElementById("watchBtn").style.display="none";
+    document.getElementById("clickBtn").style.display="block";
+  }
 }
 
-// CLICK THIS AD (+5 TK)
-clickBtn.onclick = ()=>{
-  showPopAd();
-  startAdTimer(()=>{
-    balance += 5;
-    document.getElementById("balance").innerText = balance;
-    watched = 0;
-    watchBtn.style.display="inline-block";
-    clickBox.style.display="none";
-  });
-}
-
-// WITHDRAW
-document.getElementById("withdrawBtn").onclick = ()=>{
-  let amount = Number(document.getElementById("amount").value);
+function withdraw(){
+  let amount = parseInt(document.getElementById("amount").value);
   let acc = document.getElementById("account").value;
 
-  if(amount < 110) return alert("Minimum withdraw 110 TK");
-  if(amount > balance) return alert("Insufficient balance");
-  if(!acc) return alert("Enter account number");
+  if(amount < 110){
+    alert("Minimum withdraw 110 TK");
+    return;
+  }
+  if(amount > balance){
+    alert("Not enough balance");
+    return;
+  }
+  if(acc.length < 6){
+    alert("Enter valid account number");
+    return;
+  }
 
   balance -= amount;
   document.getElementById("balance").innerText = balance;
   alert("Withdraw Request Successful!");
-  showPopAd();
-}
 
-// POPUNDER FUNCTION
-function showPopAd(){
+  // Show popup ad on withdraw
   (function(s){
     s.dataset.zone='10501336';
     s.src='https://al5sm.com/tag.min.js';
   })(document.body.appendChild(document.createElement('script')));
 }
 
-// AUTO POP EVERY 2 MIN
+// Auto popup every 2 minutes
 setInterval(()=>{
-  showPopAd();
+  (function(s){
+    s.dataset.zone='10501336';
+    s.src='https://al5sm.com/tag.min.js';
+  })(document.body.appendChild(document.createElement('script')));
 },120000);
